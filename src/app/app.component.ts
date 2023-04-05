07e842991d707cca12cfd93922769c6c33b55fe4
JS952001 <sarkarpritam871@gmail.com>
2023-04-05 11:13:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularapp';
+  name = 'Pritam Sarkar';
+  location = 'Kolkata';
+  phonenumber = "4466778899";
 }
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  name = "Pritam";
+  phonenumber = "6677554433";
+
+  constructor() { }
+  ngOnInit(){
+
+  }
+
+
+}
+
+
+
+
+
